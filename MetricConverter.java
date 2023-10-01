f1d6fe88ec7d59850b8be7ed91814f78abcabc37
Ben Fahrmeier <144169391+BKfarm@users.noreply.github.com>
2023-10-01 11:20:05 -0400

    Update MetricConverter.java

--- MetricConverter.java
+++ MetricConverter.java
@@ -1,2 +1,25 @@
+import java.util.*;
+import java.lang.Math;
+
+public class MetricConverter
+{
+    public static void main(String[] args)
+    {
+        boolean done = false;
+        double meters = 0;
+        Scanner in = new Scanner(System.in);
+        do
+        {
+            System.out.println("Input length in meters: ");
+            if(in.hasNextDouble())
+            {
+                meters = in.nextDouble();
+                done = true;
+            }
+            in.nextLine();
+        }while(!done);
+        System.out.println("Length in miles: " + (meters / 1609));
+        System.out.println("Length in feet: " + (meters * 3.281));
+        System.out.println("Length in inches: " + (meters * 39.37));
     } 
 }
