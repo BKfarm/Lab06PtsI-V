28cfe94bece50349602a94b6c9be4697a585f17e
Ben Fahrmeier <144169391+BKfarm@users.noreply.github.com>
2023-09-30 20:25:51 -0400

    Create CtoFConverter.java

---
+++ CtoFConverter.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CtoFConverter
+{
+    public static void main(String[] args)
+    {
+    boolean done = false;
+    double startTemp = 0;
+    Scanner in = new Scanner(System.in);
+    do
+        {
+        System.out.println("Input temperature in Celsius: ");
+        if(in.hasNextDouble())
+        {
+            startTemp = in.nextDouble();
+            done = true;
+        }
+        in.nextLine();
+        }while(!done);
+        startTemp = (startTemp * 9/5) + 32;
+        System.out.println("Temperature in Fahrenheit": + startTemp);
+    }
+}
